cb117017f21163d53e5ffa4343ed05c7433a3de3
IPHUN1989 <tamas.peter.illles@gmail.com>
2024-04-19 15:48:50 +0200

    🦄 refactor(BoardGameCreator): Delete old methods
    Delete old methods for frontend checking

--- frontend/src/Pages/BoardGameCreator.jsx
+++ frontend/src/Pages/BoardGameCreator.jsx
@@ -1,27 +1,24 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import BoardGameForm from "../Components/BoardGameForm";
 
 const BoardGameCreator = () => {
-  const [games, setGames] = useState([]);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const createGame = async (boardGame) => {
+    try {
+      const userToken = localStorage.getItem("usertoken");
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      };
 
-    const createGame = async (boardGame) => {
-      try {
-        const userToken = localStorage.getItem("usertoken");
-        const headers = {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userToken}`,
-        };
-    
-        const res = await fetch("/api/games/create", {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(boardGame),
-        });
-    
-        if (res.ok) {
+      const res = await fetch("/api/games/create", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(boardGame),
+      });
+
+      if (res.ok) {
         alert("Thank you for sending in a new board game!")
         navigate("/games");
       }  
@@ -31,76 +28,22 @@ const BoardGameCreator = () => {
         });
       }
       else {
-          throw new Error(`Error creating game: ${res.statusText}`);
-        }
-      } catch (error) {
-        console.error("Error creating game: ", error);
-        throw error;
+        throw new Error(`Error creating game: ${res.statusText}`);
       }
-    };
-    
+    } catch (error) {
+      console.error("Error creating game: ", error);
+      throw error;
+    }
+  };
 
-
-    const gamesFetch = async () => {
-      try {
-        const userToken = localStorage.getItem("usertoken");
-        const headers = userToken
-          ? {
-              Authorization: `Bearer ${userToken}`,
-              "Content-Type": "application/json",
-            }
-          : { "Content-Type": "application/json" };
-    
-        const response = await fetch("/api/games", {
-          method: "GET",
-          headers: headers,
-        });
-    
-        if (response.ok) {
-          const data = await response.json();
-          setGames(data);
-        } else {
-          console.error(`Error fetching games: ${response.statusText}`);
-        }
-      } catch (error) {
-        console.error("Error fetching games: ", error);
-      }
-    };
-    
-    
-    useEffect(() => {
-      gamesFetch();
-    }, []);
   
-    const handleCreateBoardGame = (boardGame) => {
-      let gameExists = false;
-      for (let i = 0; i < games.length; i++) {
-        if (boardGame.gameName === games[i].gameName) {
-          gameExists = true;
-          break;
-        }
-      }
-    
-      if (gameExists) {
-        alert("This game already exists!");
-      } else {
-        createGame(boardGame)
-          .then(() => {
-            navigate("/games");
-          })
-          .catch((error) => {
-            console.error("Error creating game:", error);
-          });
-      }
-    };
-    
-  
-    return (
-      <BoardGameForm
-        onCancel={() => navigate("/")}
-        onSave={handleCreateBoardGame}
-      />
-    );
-} 
 
-export default BoardGameCreator
+  return (
+    <BoardGameForm
+      onCancel={() => navigate("/")}
+      onSave={createGame}
+    />
+  );
+};
+
+export default BoardGameCreator;
