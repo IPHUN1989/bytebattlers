5be0b23bbe90da583c6fc3c5e11e7c956194c07c
IPHUN1989 <tamas.peter.illles@gmail.com>
2024-04-25 14:30:17 +0200

    🐞 fix(Register): Add attributes
    Add different attributes to the form min/max, required, pattern

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Register = ({ onSave, onCancel }) => {
   const [showPassword, setShowPassword] = useState(false);
   const [showHint, setShowHint] = useState(true);
+
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -36,13 +37,28 @@ const Register = ({ onSave, onCancel }) => {
             <div className="input-box">
               <ion-icon name="user_name-outline"></ion-icon>
               <label htmlFor="name"></label>
-              <input placeholder="Username" name="name" id="name" />
+              <input
+                placeholder="Username"
+                name="name"
+                id="name"
+                minLength={2}
+                maxLength={100}
+                required
+              />
             </div>
 
             <div className="input-box">
               <ion-icon name="email-outline"></ion-icon>
               <label htmlFor="email"></label>
-              <input placeholder="E-mail" name="email" id="email" />
+              <input
+                placeholder="E-mail"
+                name="email"
+                type="email"
+                id="email"
+                pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
+                minLength={3}
+                required
+              />
             </div>
 
             <div className="input-box">
@@ -53,6 +69,9 @@ const Register = ({ onSave, onCancel }) => {
                 placeholder="Password"
                 name="password"
                 id="password"
+                pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                minLength={8}
+                maxLength={50}
                 onClick={() => setShowHint(false)}
                 required
               />
