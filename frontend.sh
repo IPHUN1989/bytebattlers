03eca83cc163ee75b076a987c8fc963a4fedb66a
IPHUN1989 <tamas.peter.illles@gmail.com>
2024-05-23 13:45:59 +0200

    fix(frontend.sh): Add automata installation of npm
    Add automata installation of npm in the script

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+installing_with_npm () {
+  cd ./frontend
+  npm install
+  cd ..
+}
+
 
 building_frontend () {
   echo "Frontend is building"
@@ -10,6 +16,8 @@ running_static_webserver () {
   echo "Webserver is running"
   npm --prefix ./frontend/ run preview
 }
+
+installing_with_npm
 
 building_frontend
 
